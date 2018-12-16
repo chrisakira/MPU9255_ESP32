a17264c4f3f4811eb32dc26e8b5b3804ce07f481
chrisakira <chris.akira.maekawa@gmail.com>
2018-12-16 11:44:14 -0200

    Sem resposta do mpu

--- lib/AkiraMpu.h
+++ lib/AkiraMpu.h
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <Wire.h>
+
 enum registers
 {
     //sensor adresses
@@ -139,13 +141,142 @@ int16_t gx=0;//X axis
 int16_t gy=0;//Y axis
 int16_t gz=0;//Z axis
 
-  //magnetometer raw data
 int16_t mx=0;//X axis
 int16_t my=0;//Y axis
 int16_t mz=0;//Z axis
 
+double mx_sensitivity;//X axis
+double my_sensitivity;//Y axis
+double mz_sensitivity;//Z axis
+
+int AX_offset;//X axis
+int AY_offset;//Y axis
+int AZ_offset;//Z axis
+
+int GX_offset;//X axis
+int GY_offset;//Y axis
+int GZ_offset;//Z axis
+
+int16_t uint8ToUint16(uint8_t Lbyte, uint8_t Hbyte)
+{
+  return ((int16_t)Hbyte << 8) | Lbyte;
+}
+
+void requestBytes(uint8_t address, uint8_t subAddress, uint8_t bytes)
+{
+  Wire.beginTransmission(address);
+  Wire.write(subAddress);
+  Wire.endTransmission(false);
+  Wire.requestFrom(address, bytes);
+}
+
+void readArray(uint8_t *output, char size)
+{
+  for(char i = 0; i<size; i++)
+  {
+    output[i] = Wire.read();//read byte and put it into rawData table
+  }
+}
+
+uint8_t read(uint8_t address, uint8_t subAddress)
+{
+  requestBytes(address,subAddress,1);//request one byte from the register
+  uint8_t data = Wire.read();//read one byte of data
+  return data;
+}
+
+void write(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+  Wire.beginTransmission(address);
+  Wire.write(subAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+void write_OR(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+  uint8_t c = read(address,subAddress);
+  c = c | data;
+  write(address,subAddress,c);
+}
+
+void write_AND(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+  uint8_t c = read(address,subAddress);
+  c = c & data;
+  write(address,subAddress,c);
+}
+
+void Hreset()
+{
+  write_OR(MPU_address,PWR_MGMT_1,1<<7);//set reset bit in PWR_MGMT_1 register
+}
+
+uint8_t testIMU()
+{
+  if(read(MPU_address,WHO_AM_I)==0xFF)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+//test magnetometer
+//Returns 0 if success, 1 if failure
+uint8_t testMag()
+{
+  if(read(MAG_address,MAG_ID)==0xFF)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+void read_acc()
+{
+  requestBytes(MPU_address, ACCEL_XOUT_H, 6);//request data from the accelerometer
+
+  //read data
+  uint8_t rawData[6];
+  readArray(rawData,6);
+
+  //store data in raw data variables
+  ax = uint8ToUint16(rawData[1], rawData[0]);
+  ay = uint8ToUint16(rawData[3], rawData[2]);
+  az = uint8ToUint16(rawData[5], rawData[4]);
+}
+
 int initMpu()
 {
-    Wire.begin();//enable I2C interface
-    Hreset();//reset the chip
+  Hreset();
+  write(MPU_address,CONFIG, 0x03); // set DLPF_CFG to 11
+  write(MPU_address,SMPLRT_DIV, 0x00);// set prescaler sample rate to 0
+  write(MPU_address,GYRO_CONFIG, 0x01);// set gyro to 3.6 KHz bandwidth, and 0.11 ms using FCHOICE bits
+  write(MPU_address,INT_PIN_CFG, 0x02);// enable bypass
+  write(MAG_address, CNTL, 0x16);//set magnetometer to read in mode 2 and enable 16 bit measurements
+
+  //read magnetometer sensitivity
+  mx_sensitivity = (((read(MAG_address, ASAX)-128)*0.5)/128)+1;
+  my_sensitivity = (((read(MAG_address, ASAY)-128)*0.5)/128)+1;
+  mz_sensitivity = (((read(MAG_address, ASAZ)-128)*0.5)/128)+1;
+
+  //read factory gyroscope offset
+  GX_offset = uint8ToUint16(read(MPU_address,XG_OFFSET_L), read(MPU_address,XG_OFFSET_H));
+  GY_offset = uint8ToUint16(read(MPU_address,YG_OFFSET_L), read(MPU_address,YG_OFFSET_H));
+  GZ_offset = uint8ToUint16(read(MPU_address,ZG_OFFSET_L), read(MPU_address,ZG_OFFSET_H));
+
+
+  //Based on http://www.digikey.com/en/pdf/i/invensense/mpu-hardware-offset-registers .
+  //read factory accelerometer offset
+
+  //read the register values and save them as a 16 bit value
+  AX_offset = (read(MPU_address,XA_OFFSET_H)<<8) | (read(MPU_address,XA_OFFSET_L));
+  AY_offset = (read(MPU_address,YA_OFFSET_H)<<8) | (read(MPU_address,YA_OFFSET_L));
+  AZ_offset = (read(MPU_address,ZA_OFFSET_H)<<8) | (read(MPU_address,ZA_OFFSET_L));
+  //shift offset values to the right to remove the LSB
+  AX_offset = AX_offset>>1;
+  AY_offset = AY_offset>>1;
+  AZ_offset = AZ_offset>>1;
+
+  return (testIMU() || testMag());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,25 @@
 #include <stdint.h>
 
 using namespace std;
-
+uint8_t state ;
 #include <C:\Users\chmae\OneDrive\Projetos esp32\Mpu9255\lib\AkiraMpu.h>
 
 void setup() {
-  Serial.begin(115200);//initialize Serial port
+  
+  Serial.begin(9600);
+  Wire.begin(22,21);
+  Serial.println();
+  delay(1000);
+  initMpu();
+  read_acc();
 }
 
 void loop() {
-  //print all data in serial monitor
+  Serial.print(ax);
+  Serial.print(ay);
+  Serial.print(az);
+  Serial.println();
   
-  delay(100);
+  delay(100 );
+
 }
